306ec274cb343088eb37ec020ee4d5d49103d5b2
ezio84 <brabus84@gmail.com>
2018-01-04 10:26:17 +0100

    Allow to suppress notifications sound/vibration if screen is ON [1/2]
    Change-Id: I279b202682939d797d3116089f50d65e3dd3eb01

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5978,6 +5978,14 @@ public final class Settings {
         public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
 
 	/**
+         * Whether to play notification sound and vibration if screen is ON
+         * 0 - never
+         * 1 - always
+         * @hide
+         */
+        public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
+
+	/**
          * Whether to display 4G icon instead LTE
          * @hide
          */
