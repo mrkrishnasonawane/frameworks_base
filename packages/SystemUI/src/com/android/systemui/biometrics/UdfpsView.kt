8e829d04291265166ab39edac5c622d7f0c20f52
Ilya Matyukhin <ilyamaty@google.com>
2021-09-16 16:33:28 -0700

    Fix NPE for UDFPS devices without HBM
    @Stallix: Adapt to A13
    
    Bug: 197723951
    Bug: 200295570
    Test: run with a null HbmProvider on a UDFPS device
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: Ia184978d453dc449a6f9f8882cf2b4205c14c1d6
    Merged-In: Id89552d3228b4770331e0eda558f0f97101ad8ca

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsView.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsView.kt
@@ -151,4 +151,32 @@ class UdfpsView(
         animationViewController?.onDisplayUnconfigured()
         mUdfpsDisplayMode?.disable(null /* onDisabled */)
     }
+
+    private fun doIlluminate(onIlluminatedRunnable: Runnable?) {
+        // TODO(b/231335067): enableHbm with halControlsIllumination=true shouldn't make sense.
+        // This only makes sense now because vendor code may rely on the side effects of enableHbm.
+        if (hbmProvider != null) {
+            hbmProvider?.enableHbm(halControlsIllumination) {
+                if (onIlluminatedRunnable != null) {
+                    if (halControlsIllumination) {
+                        onIlluminatedRunnable.run()
+                    } else {
+                        // No framework API can reliably tell when a frame reaches the panel. A timeout
+                        // is the safest solution.
+                        postDelayed(onIlluminatedRunnable, onIlluminatedDelayMs)
+                    }
+                } else {
+                    Log.w(TAG, "doIlluminate | onIlluminatedRunnable is null")
+                }
+            }
+        }
+    }
+
+    override fun stopIllumination() {
+        isIlluminationRequested = false
+        animationViewController?.onIlluminationStopped()
+        if (hbmProvider != null) {
+            hbmProvider?.disableHbm(null /* onHbmDisabled */)
+        }
+    }
 }
