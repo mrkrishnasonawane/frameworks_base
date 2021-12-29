8e2649cb8adfa05f98e2bb33f6e774899d606d12
Dil3mm4 <dil3mm4.dev@gmail.com>
2021-12-29 15:08:53 +0200

    Introduce Custom Clock Support
    Reversed engineered from latest descedant release
    
    spkal01: remove some logging simplify some code.
    
    Co-authored-by: spkal01 <kalligeross@gmail.com>
    Change-Id: I4f7c6b6133d5dae29525031fde87e6da19ee2cbd
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
@@ -13,6 +13,16 @@ import android.graphics.Paint.Style;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.database.ContentObserver;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.PowerManager;
+import android.os.SystemClock;
+import android.provider.Settings;
+import android.content.Context;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
@@ -73,7 +83,55 @@ public class KeyguardClockSwitch extends RelativeLayout {
 
     private int mClockSwitchYAmount;
     @VisibleForTesting boolean mChildrenAreLaidOut = false;
+
     @VisibleForTesting boolean mAnimateOnLayout = true;
+
+    private Handler mHandler;
+    private KeyguardUpdateMonitorCallback mInfoCallback = new KeyguardUpdateMonitorCallback() {
+        public void onLogoutEnabledChanged() {
+        }
+
+        public void onUserSwitchComplete(int i) {
+        }
+
+        public void onTimeChanged() {
+            if (mClockPlugin != null) {
+                mClockPlugin.onTimeTick();
+            }
+        }
+
+        public void onTimeZoneChanged(TimeZone timeZone) {
+            if (mClockPlugin != null) {
+                mClockPlugin.onTimeTick();
+            }
+        }
+
+        public void onKeyguardVisibilityChanged(boolean z) {
+            if (mClockPlugin != null) {
+                mClockPlugin.setTextColor(mDarkAmount > 0.0f ? -1 : mContext.getResources().getColor(17170490));
+            }
+        }
+
+        public void onStartedWakingUp() {
+            if (mClockPlugin != null) {
+                mClockPlugin.setTextColor(mDarkAmount > 0.0f ? -1 : mContext.getResources().getColor(17170490));
+                mClockPlugin.onTimeTick();
+            }
+        }
+
+        public void onFinishedGoingToSleep(int i) {
+            if (mClockPlugin != null) {
+                mClockPlugin.setTextColor(mDarkAmount > 0.0f ? -1 : mContext.getResources().getColor(17170490));
+            }
+        }
+
+        public void onStartedGoingToSleep(int i) {
+            if (mDisplayedClockSize != null) {
+                KeyguardClockSwitch keyguardClockSwitch = KeyguardClockSwitch.this;
+                setupFrames("startedGoingToSleep", mDisplayedClockSize.intValue() != 0);
+            }
+        }
+    };
 
     public KeyguardClockSwitch(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -137,12 +195,76 @@ public class KeyguardClockSwitch extends RelativeLayout {
 
         if (clock == null) {
             Log.e(TAG, "No clock being shown");
+	    return;
+	}else if (mClockPlugin != null) {
+            View smallClockView = mClockPlugin.getView();
+            if (smallClockView != null && smallClockView.getParent() == mClockFrame) {
+                mClockFrame.removeView(smallClockView);
+            }
+            View bigClockView = mClockPlugin.getBigClockView();
+            if (bigClockView != null && bigClockView.getParent() == mLargeClockFrame) {
+                mLargeClockFrame.removeView(bigClockView);
+            }
+            mClockPlugin.onDestroyView();
+            mClockPlugin = null;
+        }
+        boolean useLargeClock = false;
+        if (plugin == null) {
+            this.mKeyguardStatusArea.setVisibility(View.VISIBLE);
+            this.mClockView.setVisibility(View.VISIBLE);
+            this.mLargeClockView.setVisibility(View.VISIBLE);
+            this.mClockFrame.setVisibility(View.VISIBLE);
+            setMargins(this.mLargeClockFrame, 0, 0, 0, 0);
             return;
         }
 
         // Attach small and big clock views to hierarchy.
         mSmallClockFrame.addView(clock.getSmallClock());
         mLargeClockFrame.addView(clock.getLargeClock());
+        View smallClockView = plugin.getView();
+        if (smallClockView != null) {
+            mClockFrame.addView(smallClockView, -1,
+                    new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT));
+            mClockView.setVisibility(View.GONE);
+        }
+        View bigClockView = plugin.getBigClockView();
+        if (bigClockView != null) {
+            mLargeClockFrame.addView(bigClockView);
+            mLargeClockView.setVisibility(View.GONE);
+        }
+        mKeyguardStatusArea.setVisibility(plugin.shouldShowStatusArea() ? View.VISIBLE : View.GONE);
+        // Initialize plugin parameters.
+        mClockPlugin = plugin;
+        mClockPlugin.setStyle(getPaint().getStyle());
+        mClockPlugin.setTextColor(getCurrentTextColor());
+        mClockPlugin.setDarkAmount(mDarkAmount);
+        Boolean bool = mHasVisibleNotifications;
+        if (bool != null) {
+            useLargeClock = bool.booleanValue();
+        }
+        setupFrames("setPlugin", useLargeClock);
+        if (mColorPalette != null) {
+            mClockPlugin.setColorPalette(mSupportsDarkText, mColorPalette);
+        }
+    }
+
+    /**
+     * It will also update plugin setStyle if plugin is connected.
+     */
+    public void setStyle(Style style) {
+        if (mClockPlugin != null) {
+            mClockPlugin.setStyle(style);
+        }
+    }
+
+    /**
+     * It will also update plugin setTextColor if plugin is connected.
+     */
+    public void setTextColor(int color) {
+        if (mClockPlugin != null) {
+            mClockPlugin.setTextColor(color);
+        }
     }
 
     private void updateClockViews(boolean useLargeClock, boolean animate) {
@@ -221,6 +343,13 @@ public class KeyguardClockSwitch extends RelativeLayout {
             }
         });
         mStatusAreaAnim.start();
+        setupFrames("useLargeClock", !useLargeClock);
+    }
+
+    private void setPluginBelowKgArea() {
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(-1, -2);
+        layoutParams.addRule(3, this.mStatusArea.getId());
+        mLargeClockFrame.setLayoutParams(layoutParams);
     }
 
     /**
@@ -255,6 +384,87 @@ public class KeyguardClockSwitch extends RelativeLayout {
         }
 
         mChildrenAreLaidOut = true;
+    }
+
+    public Paint getPaint() {
+        return mClockView.getPaint();
+    }
+
+    public int getCurrentTextColor() {
+        return mClockView.getCurrentTextColor();
+    }
+
+    public float getTextSize() {
+        return mClockView.getTextSize();
+    }
+
+    /**
+     * Refresh the time of the clock, due to either time tick broadcast or doze time tick alarm.
+     */
+    public void refresh() {
+        if (mClockPlugin != null) {
+            mClockPlugin.onTimeTick();
+        }
+    }
+
+    /**
+     * Notifies that the time zone has changed.
+     */
+    public void onTimeZoneChanged(TimeZone timeZone) {
+        if (mClockPlugin != null) {
+            mClockPlugin.onTimeZoneChanged(timeZone);
+        }
+    }
+
+    /**
+     * Notifies that the time format has changed.
+     *
+     * @param timeFormat "12" for 12-hour format, "24" for 24-hour format
+     */
+    public void onTimeFormatChanged(String timeFormat) {
+        if (mClockPlugin != null) {
+            mClockPlugin.onTimeFormatChanged(timeFormat);
+        }
+    }
+
+    void updateColors(ColorExtractor.GradientColors colors) {
+        mSupportsDarkText = colors.supportsDarkText();
+        mColorPalette = colors.getColorPalette();
+        if (mClockPlugin != null) {
+            mClockPlugin.setColorPalette(mSupportsDarkText, mColorPalette);
+            this.mClockPlugin.setTextColor(this.mDarkAmount > 0.0f ? -1 : this.mContext.getResources().getColor(17170490));
+        }
+    }
+
+    private void setupFrames(String str, boolean useLargeClock) {
+        int i = 0;
+        if (useLargeClock) {
+            this.mClockFrame.setVisibility(View.VISIBLE);
+            setMargins(this.mLargeClockFrame, 0, 0, 0, 0);
+        } else if (hasCustomClock()) {
+                int dimensionPixelSize = mContext.getResources().getDisplayMetrics().heightPixels - mContext.getResources().getDimensionPixelSize(R.dimen.status_bar_height);
+                mClockFrame.setVisibility(!mClockPlugin.shouldShowClockFrame() ? View.GONE : View.VISIBLE);
+            if (mClockPlugin.shouldShowStatusArea()) {
+                setPluginBelowKgArea();
+            } else {
+                FrameLayout frameLayout = mLargeClockFrame;
+                if (mClockPlugin.usesPreferredY()) {
+                    i = mClockPlugin.getPreferredY(dimensionPixelSize);
+                }
+                setMargins(frameLayout, 0, i, 0, 0);
+                }
+            } else {
+                mClockFrame.setVisibility(View.VISIBLE);
+                setMargins(mLargeClockFrame, 0, 0, 0, 0);
+            }
+            refresh();
+    }
+
+    public void setMargins(View view, int i, int i2, int i3, int i4) {
+        if (view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ((ViewGroup.MarginLayoutParams) view.getLayoutParams()).setMargins(i, i2, i3, i4);
+            view.requestLayout(); 
+        }
     }
 
     public void dump(PrintWriter pw, String[] args) {
